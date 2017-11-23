818afeb4944179f747ea8d822de72f656b2c4b84
aleklisi <sebaczechu123@gmail.com>
2017-11-23 12:03:43 +0100

    removing unused file

--- writeToFile.erl
+++
@@ -1,9 +0,0 @@
--module(writeToFile).
--compile([export_all]).
-
-write_list_to_file(_,[]) -> io:fwrite("End");
-write_list_to_file(Filename,[H|T]) ->
-	file:write_file(Filename,H),
-	file:write_file(Filename,"\n"),
-	write_list_to_file(Filename,T).
-	
