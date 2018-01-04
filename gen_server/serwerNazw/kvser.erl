ffe453d981b44fdcc2398aa95d2d0d49ce5b6353
aleklisi <sebaczechu123@gmail.com>
2018-01-04 15:34:19 +0100

    ading gen supervisor

--- gen_server/serwerNazw/kvser.erl
+++ gen_server/serwerNazw/kvser.erl
@@ -4,7 +4,7 @@
 %cd("C:/Users/sebac/Documents/Erlang/Programs/Erlang/gen_server/serwerNazw/").
 
 -behaviour(gen_server).
--export([start_link/0, stop/0, add/1, del/1,val/1,hist/0,clr_hist/0,val_exists/1]).
+-export([start_link/0, stop/0, add/1, del/1,val/1,hist/0,clr_hist/0,val_exists/1,error_call/0]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -define(SERVER, ?MODULE).
@@ -31,6 +31,8 @@ hist() -> gen_server:call(?SERVER,hist).
 clr_hist() -> gen_server:call(?SERVER,clr_hist).
 %sprawdzanie czy wartość występuje w bazie
 val_exists(V) -> gen_server:call(?SERVER,{val_exists,V}).
+%error call
+error_call() ->  gen_server:call(?SERVER,error_call).
 %%====================================================================
 %% gen_server callbacks
 %%====================================================================
@@ -63,20 +65,23 @@ handle_call(hist,_From,{Hist,Dict}) ->
     
 handle_call(clr_hist,_From,{_Hist,Dict}) ->
     NewHist = [clr_hist],
-    {reply, ok, {NewHist,Dict}};
+    {noreply, ok, {NewHist,Dict}};
 
 handle_call({val_exists,V},_From,{Hist,Dict}) ->
     NewHist = Hist ++ [{val_exists,V}],
     Reply = lists:any(fun({_K,Val}) -> Val == V end ,maps:to_list(Dict)),
     {reply, Reply, {NewHist,Dict}};
 
+handle_call(error_call, _From, {Hist,Dict}) ->
+    NewHist = Hist ++ [error_call],
+    maps:get(1,#{}),
+    {noreply, {NewHist,Dict}};
+
 handle_call(_Request, _From, State) ->
     io:fwrite("handle_call!!!\n",[]),  
     Reply = unknownRequest,
     {reply, Reply, State}.
 
-
-%----------------------------------------------------------------------
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
--- gen_server/serwerNazw/kvspvisor.erl
+++ gen_server/serwerNazw/kvspvisor.erl
@@ -1,4 +1,47 @@
 -module(kvspvisor).
 -author('AlekLisiecki').
+-behaviour(supervisor).
+-export([start_link/0]).
+-export([init/1]).
 
--behaviour()
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+init([]) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 3,
+    MaxSecondsBetweenRestarts = 30,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000,
+    Type = worker,
+
+    AChild = {kvserName, {kvser, start_link, []},
+              Restart, Shutdown, Type, [kvser]},
+
+    {ok, {SupFlags, [AChild]}}.
+
+
+
+
+
+
+
+
+
+
